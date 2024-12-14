28572b795a8270221bbc4b726dfdfea2e72e08b6
Igboke Daniel <danieligboke669@gmail.com>
2024-12-14 16:01:01 +0100

    Updated templates and settings

--- shop/views.py
+++ shop/views.py
@@ -16,7 +16,7 @@ def product_list(request,category_slug=None):
 
 def product_detail(request,id,slug):
     product = get_object_or_404(Product,id = id, slug=slug,available=True)
-    return render(request,'shop/product/product_detail',{'product':product})
+    return render(request,'shop/product/product_detail.html',{'product':product})
 
 def home(request):
     return render(request,'shop/base.html')

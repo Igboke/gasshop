cfc1c055ecfe79a9e2dbbe378e8241abfd47f678
Igboke Daniel <danieligboke669@gmail.com>
2024-12-23 16:17:08 +0100

    removed home placeholder, fixed product_detail.html error

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 def product_list(request,category_slug=None):
     category = None
@@ -16,10 +17,12 @@ def product_list(request,category_slug=None):
 
 def product_detail(request,id,slug):
     product = get_object_or_404(Product,id = id, slug=slug,available=True)
-    return render(request,'shop/product/product_detail.html',{'product':product})
+    category_name = product.category
+    products = Product.objects.filter(category=category_name)
+    cart_product_form = CartAddProductForm()
 
-def home(request):
-    return render(request,'shop/base.html')
+    return render(request,'shop/product/product_detail.html',{'product':product,'products':products,'cart_product_form':cart_product_form})
+
 
 
 
